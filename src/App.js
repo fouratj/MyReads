dd60ae9048df063e5134b86dfeb36bca91374055
Fourat Janabi <fouratj@gmail.com>
2017-07-04 21:14:48 -0300

    server
    server persists changes on refresh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
-import { Route } from 'react-router-dom';
+import { Route, History } from 'react-router-dom';
 import './App.css'
 import Shelfs from './Shelfs';
 import Search from './Search'
@@ -10,6 +10,7 @@ class BooksApp extends React.Component {
     super(props);
     this.getBookByID = this.getBookByID.bind(this);
   }
+
   state = {
     books: []
   }
@@ -24,20 +25,15 @@ class BooksApp extends React.Component {
   }
 
   updateBook (id, shelf) {
-    console.log(id, " ", shelf)
-    var self = this;
-    BooksAPI.update(id, shelf)
+    BooksAPI.update({id: id}, shelf)
             .then( (data) => {
               this.getBookByID(id, shelf);
             });
-
-    
   }
 
   getBookByID (id, shelf) {
     BooksAPI.get(id)
             .then((data) => {
-              data["shelf"] = shelf;
               this.setState( (state) => {
                 state.books = state.books
                                         .filter( item => (data.title !== item.title))
@@ -58,14 +54,21 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
+
         <Route exact path="/" component={() => {
             return <Shelfs books={this.state.books} updateBook={this.updateBook.bind(this)} /> }
           } 
         />
-        <Route path="/addBook" component={() => {
-            return <Search updateBook={this.updateBook.bind(this)} /> }
-          } 
+
+        <Route path="/addBook" component={( {history} ) => {
+            return <Search updateBook={ () => {
+              this.updateBook.bind(this);
+              history.push('/');
+              }
+             } /> 
+          } }
         />  
+
       </div>
     )
   }
